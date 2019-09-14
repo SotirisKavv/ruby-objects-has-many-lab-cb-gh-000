8194dccf81dbaece581cf85015ecab51dddc46c7
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-14 13:34:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,13 @@ class Artist
   end
 
   def songs
-    @song
+    songs =[]
+    Song.all.each do |song|
+      if song.artist.name == self.name
+        songs << song
+      end
+    end
+    songs
   end
 
   def add_song(song)
