9e76dac8b9d126c0051be1b1aeb22f6eb71ad9da
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-14 13:30:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,24 @@
 class Song
 
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   @@all =[]
 
   def initialize(name)
     @name = name
     @@all << self
+  end
+
+  def self.all
+    @@all   
+  end
+
+  def artist_name
+    if song.artist != nil
+      song.artist.name
+    else
+      nil
+    end
   end
 
 end
